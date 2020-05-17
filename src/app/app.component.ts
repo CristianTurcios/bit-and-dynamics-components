6de9f4cc1483b0734000684e970bb82c80d9eb04
Cristian Turcios <cristian.turcios@laureate.net>
2020-05-17 02:31:23 -0600

    setting examples of how import dynamic components in angular coming from bit, another module and components which are in appModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {}
 
+export class AppComponent implements OnInit, AfterViewInit {
+  lazyComp: Promise<any>;
+  productListComponent: Promise<any>;
+  dinnercomponent: Promise<any>;
+  dinnercomponent1: Promise<any>;
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+  constructor(
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.lazyComp = import('./test/test.component').then(({ TestComponent }) => TestComponent);
+    this.productListComponent = import('@bit/bit.angular-tutorial.product-list').then(m => m.ɵa);
+    this.dinnercomponent = import('./menu/dinner/dinner.component').then(m => m.DinnerComponent);
+    this.dinnercomponent1 = import('./menu/dinner/dinner.component').then(({ DinnerComponent }) => DinnerComponent);
+  }
+}
---
+++ src/app/menu/menu.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DinnerComponent } from './dinner/dinner.component';
+
+@NgModule({
+  declarations: [DinnerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MenuModule { }
